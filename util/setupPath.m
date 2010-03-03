a3c40edbbafc8c36e84d45f7edf9ad286a717eca
mattbdunham <mattbdunham@aeb6a196-0202-11df-804c-772d9fe11005>
2010-03-03 17:25:00 +0000

    git-svn-id: http://graphviz4matlab.googlecode.com/svn/trunk@47 aeb6a196-0202-11df-804c-772d9fe11005

--- util/setupPath.m
+++ util/setupPath.m
@@ -1,15 +1,12 @@
 function gvizPath = setupPath()
 % Attempt to add GraphViz to the system path, (temporariliy within the
 % matlab session only). 
+    gvizPath = 'unknown';
     if ~ispc()
-        p = 'unknown';
         return
     end
- 
-    try
+    try  %#ok
         gvizPath = winqueryreg('HKEY_LOCAL_MACHINE','SOFTWARE\AT&T Research Labs\Graphviz', 'InstallPath');
         addtosystempath(fullfile(gvizPath, 'bin'));
-    catch ME
-        givzPath = 'unknown';
     end
 end
